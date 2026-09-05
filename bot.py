a3bacf0e1c6dba80eea4db3df0519f4d27c232e8
daniabenezer737-create <daniabenezer737@gmail.com>
2026-09-06 02:02:06 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,16 +3,16 @@ import logging
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
-logging.basicConfig(level=logging.INFO)
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    level=logging.INFO
+)
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("1v1 Betting Bot ስራ ጀምሯል!")
+    await update.message.reply_text("1v1 Betting Bot መስራት ጀምሯል!")
 
 if name == 'main':
-    token = os.getenv("BOT_TOKEN")
-    
-    if not token:
-        raise ValueError("BOT_TOKEN አልተገኘም!")
+    token = os.getenv("BOT_TOKEN", "8640582909:AAEWINflMuUocmywDZrvIMjV_up84BHDF6I")
 
     app = ApplicationBuilder().token(token).build()
     app.add_handler(CommandHandler("start", start))
