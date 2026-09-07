61802cf0f89c30587437cc916cc3a413462dc672
daniabenezer737-create <daniabenezer737@gmail.com>
2026-09-07 14:32:08 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -645,3 +645,25 @@ if __name__ == '__main__':
     print("Bot is running perfectly...")
     app.run_polling()
 
+from flask import Flask
+from threading import Thread
+import os
+
+app = Flask('')
+
+@app.route('/')
+def home():
+    return "Bot is alive!"
+
+def run():
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host='0.0.0.0', port=port)
+
+def keep_alive():
+    t = Thread(target=run)
+    t.start()
+
+# ይህንን ከታች በኮድህ መጀመሪያ ላይ ጥራው
+if name == "main":
+    keep_alive()
+    # የቦትህ ማስጀመሪያ ኮድ እዚህ ጋር ይቀጥላል
